b0b81db43e6eccf4f20d7db69f1c6bafa0296926
aantaya1 <antayaalexander@gmail.com>
2018-10-30 12:10:51 -0400

    started adding voting functionality

---
+++ assig1/app/src/main/java/com/aantaya/petwars/VoteActivity.java
@@ -0,0 +1,33 @@
+package com.aantaya.petwars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class VoteActivity extends AppCompatActivity {
+
+    private TextView voteCatName;
+    private ImageView voteCatImage;
+    private Button voteButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+
+        voteCatName = findViewById(R.id.vote_cat_name);
+        voteCatImage = findViewById(R.id.vote_image);
+        voteButton = findViewById(R.id.vote_button);
+
+        voteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Trigger intent to call upvote on cat
+                //Then generate new view for new cat
+            }
+        });
+    }
+}
